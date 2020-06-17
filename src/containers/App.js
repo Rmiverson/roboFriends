6ef0d5821d4c1697da364c482244e4b83a345abe
Riley Iverson <51393887+Rmiverson@users.noreply.github.com>
2020-06-17 13:40:24 -0700

    Added Error Boundry js file applied to cardlist in app

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import '../components/ErrorBoundry';
 import './App.css';
+import ErrorBoundry from '../components/ErrorBoundry';
 
 class App extends Component { 
     constructor() {
@@ -35,7 +37,9 @@ class App extends Component {
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filteredRobots}/>
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
